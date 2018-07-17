35b98593c22997f2521357910851550ee6f13b54
zuqini <lizuqiliang@gmail.com>
2018-07-17 14:14:24 -0400

    added hands and drawer animations

--- main.lua
+++ main.lua
@@ -1,5 +1,19 @@
 buy_bar_active = false
 
+request_drink = true
+HAND_MIN_X = -384
+hand_x = HAND_MIN_X
+
+BUY_DRAWER_WIDTH = 90
+buy_drawer_x = BUY_DRAWER_WIDTH
+
+drawer_speed = 25
+hand_speed = 30
+
+function isBuyActive()
+	return love.mouse.getX() > 680
+end
+
 function love.load()
 	cup_empty_src = love.graphics.newImage("assets/cup_empty.png")
 	cup_full_src = love.graphics.newImage("assets/cup_full.png")
@@ -8,30 +22,34 @@ function love.load()
 	jug_full_src = love.graphics.newImage("assets/coffee_jug_full.png")
 	jug_empty_src = love.graphics.newImage("assets/coffee_jug_empty.png")
 	jug_pouring_src = love.graphics.newImage("assets/coffee_jug_pouring.png")
+	hand_src = love.graphics.newImage("assets/hand.png")
 	love.graphics.setNewFont(12)
 	love.graphics.setBackgroundColor(.772,.647,.529)
 end
 
 function love.update(dt)
 	local mouse_x = love.mouse.getX()
-	if love.mouse.getX() > 680 then
-		buy_bar_active = true
+	if isBuyActive() then
+		buy_drawer_x = math.max(buy_drawer_x - drawer_speed, 0)
 	else
-		buy_bar_active = false
+		buy_drawer_x = math.min(buy_drawer_x + drawer_speed, BUY_DRAWER_WIDTH)
+	end
+	if request_drink and hand_x < 0 then
+		hand_x = math.min(hand_x + hand_speed, 0)
+	elseif request_drink == false and hand_x > HAND_MIN_X then
+		hand_x = math.max(hand_x - hand_speed, HAND_MIN_X)
 	end
 end
 
 function love.draw()
-	love.graphics.setColor(1, 1, 1, 1)
-	if buy_bar_active then
-		love.graphics.draw(cup_empty_src, 725, 50)
-		love.graphics.draw(jug_full_src, 710, 50 * 2 + 64)
-		love.graphics.draw(milk_src, 720, 50 * 3 + 64 * 2)
-		love.graphics.draw(cream_src, 720, 50 * 4 + 64 * 2 + 96)
-	end
 	-- draw table
 	love.graphics.setColor(0.282, 0.188, 0.157)
-	love.graphics.rectangle("fill", 200, 520, 400, 20, 10, 10)
-	love.graphics.rectangle("fill", 300, 530, 20, 100, 10, 10)
-	love.graphics.rectangle("fill", 500, 530, 20, 100, 10, 10)
+	love.graphics.rectangle("fill", 0, 400, 800, 300, 10, 10)
+
+	love.graphics.setColor(1, 1, 1, 1)
+	love.graphics.draw(hand_src, hand_x, 150)
+	love.graphics.draw(cup_empty_src, 725 + buy_drawer_x, 50)
+	love.graphics.draw(jug_full_src, 710 + buy_drawer_x, 50 * 2 + 64)
+	love.graphics.draw(milk_src, 720 + buy_drawer_x, 50 * 3 + 64 * 2)
+	love.graphics.draw(cream_src, 720 + buy_drawer_x, 50 * 4 + 64 * 2 + 96)
 end
