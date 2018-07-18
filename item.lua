fdaeb0ba1a4495da4d0825358956b041a3e2dde1
zuqini <lizuqiliang@gmail.com>
2018-07-17 20:38:26 -0400

    added fonts and implemented core game functionality

--- item.lua
+++ item.lua
@@ -2,18 +2,17 @@ Class = require 'modules.hump.class'
 
 Item = Class{}
 
-function Item:init(x, y, capacity, type, rsrc)
+function Item:init(x, y, capacity, amount, type, rsrc)
 	self.x = x
 	self.y = y
 	self.v = 0
+	self.r = 0
 	self.capacity = capacity
+	self.amount = amount
+	self.content = { milk = 0, cream = 0, coffee = 0 }
 	self.type = type
 	self.rsrc = rsrc
-end
-
-function Item:pour()
-	self.capacity = self.capacity - 1
-	return self.type
+	self.is_pouring = false
 end
 
 return Item
--- main.lua
+++ main.lua
@@ -11,7 +11,10 @@ local HAND_MIN_X = -384
 local BUY_DRAWER_WIDTH = 90
 local TABLE_HEIGHT = 400
 
+local money = 10
+
 local request_drink = true
+local request_content = { milk = 0, cream = 0, coffee = 0 }
 local hand_x = HAND_MIN_X
 
 local buy_drawer_x = BUY_DRAWER_WIDTH
@@ -26,6 +29,30 @@ function isBuyActive()
 	return love.mouse.getX() > 680
 end
 
+function generateRequest()
+	request_content.coffee = math.random(100)
+	request_content.milk = math.random(100 - request_content.coffee)
+	request_content.cream = 100 - request_content.coffee - request_content.milk
+end
+
+function pour(to)
+	if to.amount < to.capacity and grabbed_item.amount > 0 then
+		grabbed_item.is_pouring = true
+		grabbed_item.amount = grabbed_item.amount - 1
+		to.amount = math.min(to.amount + 1, to.capacity)
+		if grabbed_item.type == item_types.COFFEE then
+			to.content.coffee = to.content.coffee + 1
+		elseif grabbed_item.type == item_types.MILK then
+			to.content.milk = to.content.milk + 1
+		elseif grabbed_item.type == item_types.CREAM then
+			to.content.cream = to.content.cream + 1
+		end
+		if to.amount == to.capacity then
+			to.rsrc = cup_full_rsrc
+		end
+	end
+end
+
 function love.load()
 	cup_empty_rsrc = love.graphics.newImage("assets/cup_empty.png")
 	cup_full_rsrc = love.graphics.newImage("assets/cup_full.png")
@@ -35,8 +62,14 @@ function love.load()
 	jug_empty_rsrc = love.graphics.newImage("assets/coffee_jug_empty.png")
 	jug_pouring_rsrc = love.graphics.newImage("assets/coffee_jug_pouring.png")
 	hand_rsrc = love.graphics.newImage("assets/hand.png")
-	love.graphics.setNewFont(12)
+	font = love.graphics.newImageFont("assets/font.png",
+    " abcdefghijklmnopqrstuvwxyz" ..
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
+    "123456789.,!?-+/():;%&`'*#=[]\"")
+    love.graphics.setFont(font)
+	--love.graphics.setNewFont(12)
 	love.graphics.setBackgroundColor(.772,.647,.529)
+	generateRequest()
 end
 
 function love.mousepressed(x, y, button, istouch)
@@ -46,13 +79,13 @@ function love.mousepressed(x, y, button, istouch)
 				grabbed_item = nil
 			else
 				if y > 50 and y < 50 + 64 then
-					grabbed_item = Item(x, y, 0, item_types.CUP, cup_empty_rsrc)
+					grabbed_item = Item(x, y, 100, 0, item_types.CUP, cup_empty_rsrc)
 				elseif y > 50 * 2 + 64 and y < 50 * 2 + 64 * 2 then
-					grabbed_item = Item(x, y, 200, item_types.COFFEE, jug_full_rsrc)
+					grabbed_item = Item(x, y, 300, 300, item_types.COFFEE, jug_full_rsrc)
 				elseif y > 50 * 3 + 64 * 2 and y < 50 * 3 + 64 * 2 + 96 then
-					grabbed_item = Item(x, y, 200, item_types.MILK, milk_rsrc)
+					grabbed_item = Item(x, y, 100, 100, item_types.MILK, milk_rsrc)
 				elseif y > 50 * 4 + 64 * 2 + 96 and y < 50 * 4 + 64 * 2 + 96 * 2 then
-					grabbed_item = Item(x, y, 200, item_types.CREAM, cream_rsrc)
+					grabbed_item = Item(x, y, 100, 100, item_types.CREAM, cream_rsrc)
 				end
 			end
 		else
@@ -90,15 +123,56 @@ function love.update(dt)
 	if grabbed_item ~= nil then
 		grabbed_item.x = love.mouse.getX() - grabbed_item.rsrc:getWidth() / 2
 		grabbed_item.y = love.mouse.getY() - grabbed_item.rsrc:getHeight() / 2
+		grabbed_item.is_pouring = false
 	end
 
-	for _, item in ipairs(items) do
+	local item_to_remove = 0
+	for i, item in ipairs(items) do
 		if item.y + 50 < TABLE_HEIGHT then
 			item.v = item.v + 1
 		else
 			item.v = 0
+			if grabbed_item ~= nil and grabbed_item.type ~= item_types.CUP and
+					item.type == item_types.CUP then
+				if grabbed_item.x > item.x and grabbed_item.x < item.x + item.rsrc:getWidth() and
+						grabbed_item.y < item.y and grabbed_item.y > item.y - 30 then
+					pour(item)
+				end
+			end
 		end
 		item.y = item.y + item.v
+
+		if request_drink and item.type == item_types.CUP and item.amount == 100 and
+				item.x > 200 and item.x < 382 and
+				item.y + item.rsrc:getHeight() > 150 and
+				item.y + item.rsrc:getHeight() < 245 then
+			request_drink = false
+			item_to_remove = i
+		end
+	end
+	if item_to_remove > 0 then
+		table.remove(items, item_to_remove)
+	end
+
+	if grabbed_item ~= nil and grabbed_item.is_pouring then
+		grabbed_item.r = math.max(grabbed_item.r - 0.2, - math.pi / 2)
+		if grabbed_item.type == item_types.COFFEE then
+			grabbed_item.rsrc = jug_pouring_rsrc
+		end
+	elseif grabbed_item ~= nil and not grabbed_item.is_pouring then
+		grabbed_item.r = math.min(grabbed_item.r + 0.2, 0)
+		if grabbed_item.type == item_types.COFFEE then
+			if grabbed_item.amount > 0 and grabbed_item.type == item_types.COFFEE then
+				grabbed_item.rsrc = jug_full_rsrc
+			else
+				grabbed_item.rsrc = jug_empty_rsrc
+			end
+		end
+	end
+
+	if not request_drink and hand_x == HAND_MIN_X then
+		request_drink = true
+		generateRequest()
 	end
 end
 
@@ -114,11 +188,30 @@ function love.draw()
 	love.graphics.draw(milk_rsrc, 720 + buy_drawer_x, 50 * 3 + 64 * 2)
 	love.graphics.draw(cream_rsrc, 720 + buy_drawer_x, 50 * 4 + 64 * 2 + 96)
 
+	--love.graphics.setNewFont(16)
+	love.graphics.setColor(0.8, 0, 0)
+	love.graphics.print("1", 740 + buy_drawer_x, 50 + 64 + 12)
+	love.graphics.print("5", 740 + buy_drawer_x, 50 * 2 + 64 * 2 + 12)
+	love.graphics.print("3", 740 + buy_drawer_x, 50 * 3 + 64 * 2 + 96 + 12)
+	love.graphics.print("3", 740 + buy_drawer_x, 50 * 4 + 64 * 2 + 96 * 2 + 12)
+
+	--love.graphics.setNewFont(36)
+	love.graphics.setColor(0, 0.9, 0)
+	love.graphics.print(money, 20, 20)
+	--love.graphics.setNewFont(24)
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.print("GIMME " .. request_content.coffee .. "% COFFEE, " .. request_content.milk .. "% MILK, AND " .. request_content.cream .. "% CREAM!",
+		20, 75)
+
+	--love.graphics.setNewFont(12)
 	for _, item in ipairs(items) do
 		love.graphics.draw(item.rsrc, item.x, item.y)
+		love.graphics.print(item.amount, item.x, item.y + item.rsrc:getHeight())
 	end
 
+	-- love.graphics.print(love.mouse:getX() .. " " .. love.mouse:getY(), 0 ,0)
+
 	if grabbed_item ~= nil then
-		love.graphics.draw(grabbed_item.rsrc, grabbed_item.x, grabbed_item.y)
+		love.graphics.draw(grabbed_item.rsrc, grabbed_item.x, grabbed_item.y, grabbed_item.r)
 	end
 end
